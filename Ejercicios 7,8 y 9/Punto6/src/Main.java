4acb574cbd4ab7d856e979c3e22cac03614850c4
Santiago Santos <seba00001s@gmail.com>
2023-04-26 13:35:57 -0300

    Realice el sexto punto

--- Ejercicios 7,8 y 9/Punto6/src/Main.java
+++ Ejercicios 7,8 y 9/Punto6/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,6 +9,28 @@ public class Main {
         // Por último, vuelve a recorrerlo y muestra el ArrayList final. Si te atreves,
         // puedes hacerlo en menos pasos, siempre y cuando cumplas el primer "for" de relleno.
 
-        
+        ArrayList<Integer>numeros = new ArrayList<>();
+
+        cargarArrayList(numeros);
+        eliminarYImprimir(numeros);
     }
+
+    private static void cargarArrayList(ArrayList<Integer> numeros) {
+        for(int i=1;i<=10;i++){
+            numeros.add(i);
+        }
+    }
+
+    private static void eliminarYImprimir(ArrayList<Integer> numeros){
+        int count = -1;
+        for (int i=1;i<numeros.size();i++){
+
+            if(numeros.get(i) % 2 == 0){
+                numeros.remove(i);
+                count++;
+            }
+            System.out.println(numeros.get(count));
+        }
+    }
+
 }
