3e0ab6529fa047d2bf2c8a1f5c443213e2b518b4
Santiago Santos <seba00001s@gmail.com>
2023-04-26 12:30:31 -0300

    Realice el primer punto

--- Ejercicios 7,8 y 9/Punto1/src/Main.java
+++ Ejercicios 7,8 y 9/Punto1/src/Main.java
@@ -3,8 +3,13 @@ public class Main {
         //Punto 1
         // Crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.
 
-        
+        String nombres[] = {"Santiago","Pedro","Pablo","Monica","Pamela","Karina"};
+        print(nombres);
     }
 
-
+    public static void print(String[] nombres){
+        for (int i=0; i<nombres.length;i++){
+            System.out.println(nombres[i]);
+        }
+    }
 }
---
+++ Ejercicios 7,8 y 9/Punto2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Punto 2
+        // Crea un array bidimensional de enteros y recórrelo, mostrando la posición
+        // y el valor de cada elemento en ambas dimensiones.
+
+        
+    }
+}
